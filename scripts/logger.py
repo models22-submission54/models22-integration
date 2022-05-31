4606d04d517bcb8022d4f33a5f565d1a9de6c24f
Anonymous Author(s) <models22.submission54@gmail.com>
2022-05-31 16:01:43 +0700

    Adding LOGGER

---
+++ scripts/logger.py
@@ -0,0 +1,68 @@
+from datetime import datetime
+
+class SubGoalManager:
+    subgoal1: bool = False
+    subgoal2: bool = False
+    subgoal3: bool = False
+    current_subgoal: int = 1
+
+    def reset(self):
+        self.current_subgoal = 1
+        self.subgoal1 = False
+        self.subgoal2 = False
+        self.subgoal3 = False
+
+    def print_status(self, status):
+        return u'\u2713' if status else '?'
+
+    def print_conclusion(self):
+        if self.current_subgoal < 4:
+            return f"Proving subgoal C{self.current_subgoal}..."
+        else:
+            return  "Done.                                     "
+
+    def getInfo(self):
+        msg = f"SubgoalC1: {self.print_status(self.subgoal1)} SubgoalC2: {self.print_status(self.subgoal2)} SubgoalC3: {self.print_status(self.subgoal3)} > {self.print_conclusion()}"
+        return msg
+
+    def update_status(self, id, status):
+        if id == 1:
+            self.subgoal1 = status
+        elif id == 2:
+            self.subgoal2 = status
+        else:
+            self.subgoal3 = status
+
+SGM = SubGoalManager()
+
+class Logger:
+
+    msg: str = ""
+    # iteration
+    cases: int = 0
+
+    def __init__(self) -> None:
+        pass
+
+    def info(self, src, msg, end="\n") -> None:
+        print(f"INFO: [{src}] {msg}", end=end, flush=True)
+
+    # ==============================================================================================
+
+    def run_start(self, cases: int, start_time):
+        self.start_time = start_time
+        self.cases == cases
+        self.info("prover", start_time.strftime('Starting at %H:%M:%S'))
+
+    def begin_proving_round(self, round_id):
+        print("\n\n================================ Testcase {:02d} ==================================".format(round_id))
+        SGM.reset()
+    
+    def print_stat(self):
+        msg = SGM.getInfo()
+        print(f"{msg}", end = "\r", flush=True)
+
+    def finish(self):
+        print("\n\n=================================== End ======================================")
+
+LOGGER = Logger()
--- scripts/run.py
+++ scripts/run.py
@@ -4,15 +4,24 @@
 
 """
 import argparse
+from datetime import datetime
 import os
 import shutil
 import subprocess
-import sys
 import signal
 import configparser as ConfigParser
 import json
+from time import sleep
+from logger import LOGGER, SGM
+
+from numpy import true_divide
 
 BASE_DIRECTORY = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROOF_GOAL_1 = "(assert (not (exists ((x Int)) (and (index0 x) (forall ((y Int)) (=> (not (= x y)) (not (index0 y))))))))\r\n"
+PROOF_GOAL_2_OCL = "(assert (not {0}))\r\n"
+PROOF_GOAL_2_SQL = "(assert (forall ((x Int)) (=> (index0 x) (= (val-index0-res x) TRUE))))\r\n"
+PROOF_GOAL_3_OCL = "(assert {0})\r\n"
+PROOF_GOAL_3_SQL = "(assert (not (forall ((x Int)) (=> (index0 x) (= (val-index0-res x) TRUE)))))\r\n"
 
 print("Running integration with root directory " + BASE_DIRECTORY)
 
@@ -21,7 +30,7 @@ class JSONObject(object):
         self.__dict__ = d
 
 
-def build(conf, skip_tests=False):
+def build(conf):
     """
     Builds all solutions
     """
@@ -30,113 +39,159 @@ def build(conf, skip_tests=False):
         config = ConfigParser.ConfigParser()
         config.read(os.path.join(BASE_DIRECTORY, "tools", tool, "solution.ini"))
         set_working_directory("tools", tool)
-        if skip_tests:
-            subprocess.check_call(config.get('build', 'skipTests'), shell=True)
-        else:
-            subprocess.check_call(config.get('build', 'default'), shell=True)
+        subprocess.check_call(config.get('build', 'default'), shell=True)
 
 
 def run(conf):
     """
     Integration
     """
+    start_time = datetime.today()
+    LOGGER.run_start(len(conf.Testcases), start_time)
     datamodel = os.path.join(BASE_DIRECTORY, "resources", conf.Datamodel)
     os.environ['DM'] = os.path.abspath(datamodel)
-    ocl2msfol = conf.OCL2MSFOL
-    ocl2msfol_config = ConfigParser.ConfigParser()
-    ocl2msfol_config .read(os.path.join(BASE_DIRECTORY, "tools", ocl2msfol, "solution.ini"))
-    sql2msfol = conf.SQL2MSFOL
-    sql2msfol_config = ConfigParser.ConfigParser()
-    sql2msfol_config .read(os.path.join(BASE_DIRECTORY, "tools", sql2msfol, "solution.ini"))
     for iTestcase, testcase in enumerate(conf.Testcases):
-        print("Generating theory for testcase {0}".format(str(iTestcase)))
-        ctx = ''
-        for var in testcase.context:
-            if ctx == '':
-                ctx += '{0}:{1}'.format(var.name, var.type)
-            else:
-                ctx += ',{0}:{1}'.format(var.name, var.type)
-        if ctx != '':
-            os.environ['CTX'] = ctx
-        inv = ''
-        for invariant in testcase.invariants:
-            if inv == '':
-                inv += invariant
-            else:
-                inv += ','+invariant
-        if inv != '':         
-            os.environ['INV'] = inv
-        os.environ['OCL'] = testcase.OCL
-        os.environ['SQL'] = testcase.SQL
-        filename = 'output{0}'.format(str(iTestcase))
-        os.environ['FILE'] = filename
+        LOGGER.begin_proving_round(iTestcase)
+        LOGGER.print_stat()
+        filename = setting_ENV_variables(testcase, iTestcase)
+
         header = os.path.join(BASE_DIRECTORY, "output", "header.smt2")
-        result_file = os.path.join(BASE_DIRECTORY, "output", filename + ".smt2")
-        if os.path.exists(result_file):
-            os.remove(result_file)
-        shutil.copy(header, result_file)
 
-        set_working_directory("tools", ocl2msfol)
-        with subprocess.Popen(ocl2msfol_config.get('run', 'cmd'), shell=True, stdout=subprocess.PIPE,
-                            start_new_session=True) as process:
-            try:
-                stdout, stderr = process.communicate(timeout=conf.Timeout)
-                return_code = process.poll()
-                if return_code:
-                    raise subprocess.CalledProcessError(return_code, process.args,
-                                                        output=stdout, stderr=stderr)
-            except subprocess.TimeoutExpired:
-                os.killpg(process.pid, signal.SIGINT)  # send signal to the process group
-                raise
+        core_theory_filename = os.path.join(BASE_DIRECTORY, "output", filename + ".smt2")
+        if os.path.exists(core_theory_filename):
+            os.remove(core_theory_filename)
+        shutil.copy(header, core_theory_filename)
 
-        ocl_formula = stdout
-        ocl_theory_file = os.path.join(BASE_DIRECTORY, "tools", ocl2msfol, filename)
-        with open(ocl_theory_file,'r') as sourcefile, open(result_file,'a') as targetfile:
-            for line in sourcefile:
-                targetfile.write(line)
-        os.remove(ocl_theory_file)
+        if 'cvc4' == SOLVER:
+            cvc4_fragment = '(set-logic UFSLIA)\r\n'
+            with open(core_theory_filename, "ab") as file:
+                file.write(cvc4_fragment.encode())
 
-        set_working_directory("tools", sql2msfol)
-        with subprocess.Popen(sql2msfol_config.get('run', 'cmd'), shell=True, stdout=subprocess.PIPE,
-                            start_new_session=True) as process:
-            try:
-                stdout, stderr = process.communicate(timeout=conf.Timeout)
-                return_code = process.poll()
-                if return_code:
-                    raise subprocess.CalledProcessError(return_code, process.args,
-                                                        output=stdout, stderr=stderr)
-            except subprocess.TimeoutExpired:
-                os.killpg(process.pid, signal.SIGINT)  # send signal to the process group
-                raise
-        with open(result_file, "ab") as file:
-            file.write(stdout)
+        ocl_formula = ocl2msfol(conf, core_theory_filename, filename)
+        sql2msfol(conf, core_theory_filename)
 
         # Adding proof goals
         set_working_directory("output")
-        proofgoal = "(assert (not (exists ((x Int)) (and (index0 x) (forall ((y Int)) (=> (not (= x y)) (not (index0 y))))))))\r\n"  
-        proof_file1 = filename + '-C1.smt2'
-        shutil.copy(result_file, proof_file1)
-        with open(proof_file1, "ab") as file:
+        add_proof_goal(conf, [PROOF_GOAL_1], filename, core_theory_filename, 1)
+        add_proof_goal(conf, [PROOF_GOAL_2_OCL.format(ocl_formula), PROOF_GOAL_2_SQL], filename, core_theory_filename, 2)
+        add_proof_goal(conf, [PROOF_GOAL_3_OCL.format(ocl_formula), PROOF_GOAL_3_SQL], filename, core_theory_filename, 3)
+    LOGGER.finish()
+
+def setting_ENV_variables(testcase, iTestcase):
+    ctx = ''
+    for var in testcase.context:
+        if ctx == '': ctx += '{0}:{1}'.format(var.name, var.type)
+        else: ctx += ',{0}:{1}'.format(var.name, var.type)
+    if ctx != '':  os.environ['CTX'] = ctx
+    inv = ",". join(testcase.invariants)
+    if inv != '': os.environ['INV'] = inv
+    os.environ['OCL'] = testcase.OCL
+    os.environ['SQL'] = testcase.SQL
+    filename = 'output{0}'.format(str(iTestcase))
+    os.environ['FILE'] = filename
+    return filename
+
+def ocl2msfol(conf, core_filename, filename):
+    ocl2msfol = conf.OCL2MSFOL
+    set_working_directory("tools", ocl2msfol)
+    ocl2msfol_config = ConfigParser.ConfigParser()
+    ocl2msfol_config.read(os.path.join(BASE_DIRECTORY, "tools", ocl2msfol, "solution.ini"))
+    with subprocess.Popen(ocl2msfol_config.get('run', 'cmd'), shell=True, stdout=subprocess.PIPE,
+                            start_new_session=True) as process:
+        try:
+            stdout, stderr = process.communicate(timeout=conf.Timeout)
+            return_code = process.poll()
+            if return_code:
+                raise subprocess.CalledProcessError(return_code, process.args,
+                                                    output=stdout, stderr=stderr)
+        except subprocess.TimeoutExpired:
+            os.killpg(process.pid, signal.SIGINT)  # send signal to the process group
+            raise
+
+    ocl_formula = stdout
+    ocl_theory_file = os.path.join(BASE_DIRECTORY, "tools", ocl2msfol, filename)
+    with open(ocl_theory_file,'r') as sourcefile, open(core_filename,'a') as targetfile:
+        for line in sourcefile:
+            targetfile.write(line)
+    os.remove(ocl_theory_file)
+    return ocl_formula.decode("utf-8").replace("\r\n","").replace("\n","")
+
+
+def sql2msfol(conf, filename):
+    sql2msfol = conf.SQL2MSFOL
+    set_working_directory("tools", sql2msfol)
+    sql2msfol_config = ConfigParser.ConfigParser()
+    sql2msfol_config .read(os.path.join(BASE_DIRECTORY, "tools", sql2msfol, "solution.ini"))
+    with subprocess.Popen(sql2msfol_config.get('run', 'cmd'), shell=True, stdout=subprocess.PIPE,
+                            start_new_session=True) as process:
+        try:
+            stdout, stderr = process.communicate(timeout=conf.Timeout)
+            return_code = process.poll()
+            if return_code:
+                raise subprocess.CalledProcessError(return_code, process.args,
+                                                    output=stdout, stderr=stderr)
+        except subprocess.TimeoutExpired:
+            os.killpg(process.pid, signal.SIGINT)  # send signal to the process group
+            raise
+    with open(filename, "ab") as file:
+        file.write(stdout)
+        file.close()
+
+def add_proof_goal(conf, proofgoals, filename, corefile, id):
+    filename = filename + '-C{0}.smt2'.format(str(id))
+    shutil.copy(corefile, filename)
+    with open(filename, "ab") as file:
+        for proofgoal in proofgoals:
             file.write(proofgoal.encode())
-            file.write("(check-sat)\r\n".encode())
+        file.write("(check-sat)\r\n".encode())
+        file.close()
+    if SOLVER == 'cvc4':
+        cvc4_checksat(conf, filename, id)
+    else:
+        z3_checksat(conf, filename, id)
 
-        negate_proofgoal_ocl = "(assert (not {0}))\r\n".format(ocl_formula.decode("utf-8").replace("\r\n","")) 
-        proofgoal_sql = "(assert (forall ((x Int)) (=> (index0 x) (= (val-index0-res x) TRUE))))\r\n"
-        proof_file2 = filename + '-C2.smt2'
-        shutil.copy(result_file, proof_file2)
-        with open(proof_file2, "ab") as file:
-            file.write(negate_proofgoal_ocl.encode())
-            file.write(proofgoal_sql.encode())
-            file.write("(check-sat)\r\n".encode())
 
-        proofgoal_ocl = "(assert {0})\r\n".format(ocl_formula.decode("utf-8").replace("\r\n","")) 
-        negate_proofgoal_sql = "(assert (not (forall ((x Int)) (=> (index0 x) (= (val-index0-res x) TRUE)))))\r\n"
-        proof_file3 = filename + '-C3.smt2'
-        shutil.copy(result_file, proof_file3)
-        with open(proof_file3, "ab") as file:
-            file.write(proofgoal_ocl.encode())
-            file.write(negate_proofgoal_sql.encode())
-            file.write("(check-sat)\r\n".encode())
+def cvc4_checksat(conf, filename, id):
+    with subprocess.Popen(['cvc4 ' + filename], shell=True, stdout=subprocess.PIPE,
+                            start_new_session=True) as process:
+        try:
+            stdout, stderr = process.communicate(timeout=conf.Timeout)
+            return_code = process.poll()
+            if return_code:
+                raise subprocess.CalledProcessError(return_code, process.args,
+                                                    output=stdout, stderr=stderr)
+        except subprocess.TimeoutExpired:
+            os.killpg(process.pid, signal.SIGINT)  # send signal to the process group
+            raise
+    result = stdout.decode("utf-8").replace("\r\n","").replace("\n","")
+    if result == 'unsat': 
+        SGM.update_status(id, True)
+    else: 
+        SGM.update_status(id, False)
+    SGM.current_subgoal += 1
+    LOGGER.print_stat()
+
+
+def z3_checksat(conf, filename, id):
+    with subprocess.Popen(['z3 ' + filename], shell=True, stdout=subprocess.PIPE,
+                            start_new_session=True) as process:
+        try:
+            stdout, stderr = process.communicate(timeout=conf.Timeout)
+            return_code = process.poll()
+            if return_code:
+                raise subprocess.CalledProcessError(return_code, process.args,
+                                                    output=stdout, stderr=stderr)
+        except subprocess.TimeoutExpired:
+            os.killpg(process.pid, signal.SIGINT)  # send signal to the process group
+            raise
+    result = stdout.decode("utf-8").replace("\r\n","").replace("\n","")
+    if result == 'unsat': 
+        SGM.update_status(id, True)
+    else: 
+        SGM.update_status(id, False)
+    SGM.current_subgoal += 1
+    LOGGER.print_stat()
+
 
 def clean_dir(*path):
     dir = os.path.join(BASE_DIRECTORY, *path)
@@ -176,25 +231,23 @@ if __name__ == "__main__":
     parser.add_argument("-r", "--run",
                         help="run the integration",
                         action="store_true")
-    parser.add_argument("-s", "--skip-tests",
-                        help="skip tests",
-                        action="store_true")
-    parser.add_argument("-v", "--visualize",
-                        help="create visualizations",
-                        action="store_true")
-    parser.add_argument("-t", "--test",
-                        help="run test",
-                        action="store_true")
-    parser.add_argument("-d", "--debug",
-                        help="set debug to true",
-                        action="store_true")
     parser.add_argument('-c', '--testcase', 
                         action='store', 
                         dest='testcase', 
                         help='The testcase filename.')
+    parser.add_argument('-s', '--solver', 
+                        action='store', 
+                        dest='solver', 
+                        default='z3',
+                        help='The solver of choice.')
+    parser.add_argument('--get-model',
+                        action='store_true',
+                        dest='model',
+                        help='get model when the proofgoal is unsuccessful')
     args = parser.parse_args()
 
     FILE_NAME = args.testcase
+    SOLVER = args.solver
 
     set_working_directory("config")
     with open(FILE_NAME+".json", "r") as config_file:
@@ -204,12 +257,7 @@ if __name__ == "__main__":
     # with the test and the visualization/reporting
     no_args = all(not val for val in vars(args).values())
 
-    if args.debug:
-        os.environ['DEBUG'] = 'true'
-    if args.build or args.test or no_args:
-        build(config, args.skip_tests and not args.test)
+    if args.build or no_args:
+        build(config)
     if args.run or no_args:
         run(config)
-    # not yet supported!
-    if args.visualize or no_args:
-        visualize() 
